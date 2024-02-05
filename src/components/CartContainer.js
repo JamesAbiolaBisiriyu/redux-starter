d14f006e58d3034d92d68403520d8dffe5ffe40d
james <emperor2g@gmail.com>
2024-02-06 00:26:10 +0100

    modalcomponent

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -31,7 +31,7 @@ if (amount<1){
       <footer>
         <hr/>
         <div className='cart-total'>
-        <h4> Total<span> ${total}</span></h4>
+        <h4> Total<span> ${total.toFixed(2)}</span></h4>
         </div>
         <button className='btn clear-btn' onClick={()=>dispatch(clearCart())}>Clear Cart</button>
       </footer>
